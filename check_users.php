8458309df6e119d2c3452dc7f0b736b9d80db728
CRIMSON ARCHITECTS <crimson@lobby69.dev>
2026-07-29 23:24:37 -0600

    chore: eliminar archivos de diagnostico temporales

--- check_users.php
+++
@@ -1,6 +0,0 @@
-DB::table('users')
-  ->leftJoin('memberships', 'users.id', '=', 'memberships.user_id')
-  ->select('users.username', 'memberships.membership_type', 'memberships.status', 'memberships.expires_at')
-  ->whereIn('users.username', ['Luna_MX', 'ParejaCDMX2', 'Single_uno'])
-  ->get()
-  ->each(function($u){ echo $u->username.' | '.$u->membership_type.' | '.$u->status.' | '.$u->expires_at.PHP_EOL; });
--- diag_users.php
+++
@@ -1,8 +0,0 @@
-<?php
-$users = DB::table('users')
-    ->whereIn('username', ['Luna_MX', 'ParejaCDMX2', 'Single_uno'])
-    ->select('id', 'username', 'membership_type', 'email')
-    ->get();
-foreach ($users as $u) {
-    echo $u->username . ' | ' . ($u->membership_type ?? 'NULL') . ' | id=' . $u->id . PHP_EOL;
-}
